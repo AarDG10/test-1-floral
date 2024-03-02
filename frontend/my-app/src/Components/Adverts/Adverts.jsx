a54c4f82dae80d723a5769b67b132613ea4bb9be
Aarol D'Souza <aarol.dsouza@gmail.com>
2024-03-02 17:39:23 +0530

    New updates added

--- frontend/my-app/src/Components/Adverts/Adverts.jsx
+++ frontend/my-app/src/Components/Adverts/Adverts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Adverts.css'
 import bgimg from '../Assets/Vert-home.png'
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 const Adverts = () => {
   return (
@@ -9,7 +10,7 @@ const Adverts = () => {
         <h2>Super Value Deals</h2>
         <h1>On all Products</h1>
         <p>Save More with Coupons & up to 70% off!</p>
-        <button>Shop Now</button>
+        <Link to='/shop'><button>Shop Now</button></Link>
     </section>
   )
 }
--- frontend/my-app/src/Pages/Home.jsx
+++ frontend/my-app/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner1 from '../Components/Adverts/Adverts';
 import Feat1 from '../Components/Features/Features';
 import Newsletter from '../Components/Newsletter/Newsletter';
+import Popular from '../Components/Popular/Popular';
 
 
 export const Home = () => {
@@ -9,6 +10,7 @@ export const Home = () => {
     <div>
         <Banner1/>
         <Feat1/>
+        <Popular/>
         <Newsletter/>
     </div>
   )
