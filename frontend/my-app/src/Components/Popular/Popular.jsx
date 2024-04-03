3dd77011b286ba87e13cbab92bc5e99b1f90f4f4
Aarol D'Souza <aarol.dsouza@gmail.com>
2024-04-03 19:54:27 +0530

    CSS fix for cards

--- frontend/my-app/src/Components/Popular/Popular.jsx
+++ frontend/my-app/src/Components/Popular/Popular.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { createContext, useContext , useState, useEffect} from 'react'
 import './Popular.css'
-import { Unlayer } from './Unlayer'
-import { Deuxlayer } from './Deuxlayer'
+import Item from '../Item/Item'
 
 const Popular = () => {
+  const [all_product,setAll_Product] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:4000/allproducts')
+    .then((response)=>response.json())
+    .then((data)=>setAll_Product(data))
+  },[])
   return (
     <div className="popular">
         <h1>Popular in&nbsp;<span> Vert</span></h1>
         <hr/>
-        <Unlayer/>
-        <Deuxlayer/>
+        <div className="popular-item">
+            {all_product.map((item,i)=>{
+                return i>0? <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> :null;
+            })
+           }
+      </div>
     </div>
   )
 }
