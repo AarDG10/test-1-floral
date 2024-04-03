1a40d5360289d1133c99a2f7087fc27625115813
Aarol D'Souza <aarol.dsouza@gmail.com>
2024-04-03 17:44:25 +0530

    Login/Signup Feat Updated

--- frontend/my-app/src/Components/Navbar/Navbar.jsx
+++ frontend/my-app/src/Components/Navbar/Navbar.jsx
@@ -18,7 +18,9 @@ export const Navbar = () => {
                 <li><Link to='/herb'>Herbs</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/login'>Login</Link></li>
+                {localStorage.getItem('auth-token')
+                ?<li><Link onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</Link></li>
+              :<li><Link to='/login'>Login</Link></li>}
             </ul>
             <div className="nav-login-cart">
                 <Link to='/cart'><img src={cart_icon} alt="Cart"></img></Link>
